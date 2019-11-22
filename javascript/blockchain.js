f8745869d7b7fadc80af0e145721b62b331673c9
SamuelMSD <56126328+SamuelMSD@users.noreply.github.com>
2019-11-21 21:06:12 -0300

    Add files via upload

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -1,4 +1,4 @@
-var enderecoContrato = "0x53C34f4897a4111f9299a9b734aD1d5c380a8Ce5";
+var enderecoContrato = "0x46c19b692D6a8a9051f8A8CE38539606f41DAa8a";
 var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
 ethereum.enable();
 var signatario = provedor.getSigner();
